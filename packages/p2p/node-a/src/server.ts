000a0af4034bc45f59fdfc8c57c257016c97d159
sanjay-del <sanjaypantha.np@gmail.com>
2023-04-27 15:19:19 +0545

    Fastify warning removal

--- packages/p2p/node-a/src/server.ts
+++ packages/p2p/node-a/src/server.ts
@@ -2,12 +2,9 @@ import * as dotenv from "dotenv"
 dotenv.config()
 import { AppConfig } from "./config.js"
 import Fastify, { FastifyInstance } from "fastify"
-const server: FastifyInstance = Fastify()
+const app: FastifyInstance = Fastify()
 
-server.listen(AppConfig.app.port, "0.0.0.0", 100000, async () => {
-  try {
-    console.log(`node 1 started at ${AppConfig.app.domain}`)
-  } catch (err) {
-    console.log(err)
-  }
+app.listen({ port: AppConfig.app.port }, (err) => {
+  if (err) throw err
+  console.log(`node 1 started at ${AppConfig.app.port}`)
 })
